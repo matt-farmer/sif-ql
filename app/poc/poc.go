a0113b1d76f62b90e457c560a533e1212b913aca
matt-farmer <mattfarmer@matts-MacBook-Air.local>
2017-05-10 09:48:26 +1000

    SIF like objects
    first experiment with object based on sif xml structure

--- app/poc/poc.go
+++ app/poc/poc.go
@@ -1,66 +1,92 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
+	"github.com/nsip/nias2/xml"
 	"github.com/playlyfe/go-graphql"
 )
 
 func main() {
+
 	schema := `
-	interface Pet {
-	    name: String
+	type NAPEvent {
+		EventID: String
+		SPRefID: String
+		PSI: String
+		#SchoolRefID: String
+		#SchoolID: String
 	}
-	type Dog implements Pet {
-	    name: String
-	    woofs: Boolean
-	}
-	type Cat implements Pet {
-	    name: String
-	    meows: Boolean
-	}
-	type QueryRoot {
-	    pets: [Pet]
+	type School {
+		events: [NAPEvent]
 	}
 	`
-	resolvers := map[string]interface{}{}
-	resolvers["QueryRoot/pets"] = func(params *graphql.ResolveParams) (interface{}, error) {
-		return []map[string]interface{}{
-			{
-				"__typename": "Dog",
-				"name":       "Odie",
-				"woofs":      true,
-			},
-			{
-				"__typename": "Cat",
-				"name":       "Garfield",
-				"meows":      false,
-			},
-		}, nil
+	event := &xml.NAPEvent{
+		EventID:     "event1",
+		SPRefID:     "student1",
+		PSI:         "psi1",
+		SchoolRefID: "school_refid1",
+		SchoolID:    "12345",
 	}
+
+	resolvers := map[string]interface{}{}
+	resolvers["NAPEvent/EventID"] = func(params *graphql.ResolveParams) (interface{}, error) {
+
+		if napEvent, ok := params.Source.(*xml.NAPEvent); ok {
+			return napEvent.EventID + ":source", nil
+		} else {
+			return event.EventID + ":var", nil
+		}
+	}
+	resolvers["NAPEvent/SPRefID"] = func(params *graphql.ResolveParams) (interface{}, error) {
+
+		if napEvent, ok := params.Source.(*xml.NAPEvent); ok {
+			return napEvent.SPRefID + ":source", nil
+		} else {
+			return event.SPRefID + ":var", nil
+		}
+	}
+	resolvers["NAPEvent/PSI"] = func(params *graphql.ResolveParams) (interface{}, error) {
+
+		if napEvent, ok := params.Source.(*xml.NAPEvent); ok {
+			return napEvent.PSI + ":source", nil
+		} else {
+			return event.PSI + ":var", nil
+		}
+	}
+
+	resolvers["School/events"] = func(params *graphql.ResolveParams) (interface{}, error) {
+
+		return []interface{}{event, event}, nil
+
+	}
+
 	context := map[string]interface{}{}
 	variables := map[string]interface{}{}
-	executor, err := graphql.NewExecutor(schema, "QueryRoot", "", resolvers)
+	executor, err := graphql.NewExecutor(schema, "School", "", resolvers)
 	executor.ResolveType = func(value interface{}) string {
-		if object, ok := value.(map[string]interface{}); ok {
-			return object["__typename"].(string)
+		switch value.(type) {
+		case *xml.NAPEvent:
+			return "NAPEvent"
 		}
 		return ""
 	}
-	query := `{
-		pets {
-			name
-			... on Dog {
-				woofs
-			}
-			... on Cat {
-				meows
-			}
+	query := `
+	{
+		events {
+			EventID
+			SPRefID
+			PSI
 		}
 	}`
 	result, err := executor.Execute(context, query, variables, "")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("\n\n %v \n\n", result)
+
+	jsonResult, err := json.Marshal(result)
+
+	fmt.Println(string(jsonResult), "\n", err)
+
 }
