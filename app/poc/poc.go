cb1f832db6460cc2c68bd247e30ebef971047b61
matt-farmer <mattfarmer@matts-MacBook-Air.local>
2017-05-19 10:33:45 +1000

    poc update
    full round-trip and intermediate handling to resolve structural
    impedance xml-go-json

--- app/poc/poc.go
+++ app/poc/poc.go
@@ -4,61 +4,115 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"github.com/nsip/nias2/naprr"
 	"github.com/nsip/nias2/xml"
 	"github.com/playlyfe/go-graphql"
 )
 
 func main() {
 
+	sr := naprr.NewStreamReader()
+	sd := sr.GetSchoolData("21212")
+
 	schema := `
+	type TestDisruption {
+		Event: String
+	}
+
 	type NAPEvent {
 		EventID: String
 		SPRefID: String
 		PSI: String
-		#SchoolRefID: String
-		#SchoolID: String
+		SchoolRefID: String
+		SchoolID: String
+		TestID: String
+		NAPTestLocalID: String
+		TestDisruptionList: [TestDisruption]
 	}
+
 	type School {
 		events: [NAPEvent]
 	}
 	`
-	event := &xml.NAPEvent{
-		EventID:     "event1",
-		SPRefID:     "student1",
-		PSI:         "psi1",
-		SchoolRefID: "school_refid1",
-		SchoolID:    "12345",
-	}
+	// event := &xml.NAPEvent{
+	// 	EventID:     "event1",
+	// 	SPRefID:     "student1",
+	// 	PSI:         "psi1",
+	// 	SchoolRefID: "school_refid1",
+	// 	SchoolID:    "123456",
+	// }
+
+	// fmt.Printf("\n\nTestDisruptionList: %#v\n\n", event.TestDisruptionList)
+	// fmt.Printf("\n\nTestDisruption: %#v\n\n", event.TestDisruptionList.TestDisruption)
+	// fmt.Printf("\n\nEvent: %#v\n\n", event)
+
+	// tdl := struct {
+	// 	TestDisruption []struct {
+	// 		Event string
+	// 	}
+	// }{
+	// 	// {
+	// 	[]struct{}{{Event: "e1"}},
+	// 	// },
+	// }
 
 	resolvers := map[string]interface{}{}
-	resolvers["NAPEvent/EventID"] = func(params *graphql.ResolveParams) (interface{}, error) {
 
-		if napEvent, ok := params.Source.(*xml.NAPEvent); ok {
-			return napEvent.EventID + ":source", nil
-		} else {
-			return event.EventID + ":var", nil
+	resolvers["NAPEvent/TestDisruptionList"] = func(params *graphql.ResolveParams) (interface{}, error) {
+		disruptionEvents := []interface{}{}
+		if napEvent, ok := params.Source.(xml.NAPEvent); ok {
+			for _, event := range napEvent.TestDisruptionList.TestDisruption {
+				disruptionEvents = append(disruptionEvents, event)
+			}
 		}
+		return disruptionEvents, nil
 	}
-	resolvers["NAPEvent/SPRefID"] = func(params *graphql.ResolveParams) (interface{}, error) {
 
-		if napEvent, ok := params.Source.(*xml.NAPEvent); ok {
-			return napEvent.SPRefID + ":source", nil
-		} else {
-			return event.SPRefID + ":var", nil
-		}
-	}
-	resolvers["NAPEvent/PSI"] = func(params *graphql.ResolveParams) (interface{}, error) {
+	// resolvers["TestDisruption"] = func(params *graphql.ResolveParams) (interface{}, error) {
 
-		if napEvent, ok := params.Source.(*xml.NAPEvent); ok {
-			return napEvent.PSI + ":source", nil
-		} else {
-			return event.PSI + ":var", nil
-		}
-	}
+	// 	fmt.Println("\n\n-- TD got Called --\n\n")
+
+	// 	if napEvent, ok := params.Source.(TestDisruption); ok {
+	// 		return napEvent, nil
+	// 	} else {
+	// 		return "empty:var", nil
+	// 	}
+	// }
+
+	// resolvers["NAPEvent/PSI"] = func(params *graphql.ResolveParams) (interface{}, error) {
+
+	// 	if napEvent, ok := params.Source.(*xml.NAPEvent); ok {
+	// 		return napEvent.PSI + ":source", nil
+	// 	} else {
+	// 		return event.PSI + ":var", nil
+	// 	}
+	// }
+	// resolvers["NAPEvent/SchoolRefID"] = func(params *graphql.ResolveParams) (interface{}, error) {
+
+	// 	if napEvent, ok := params.Source.(*xml.NAPEvent); ok {
+	// 		return napEvent.SchoolRefID + ":source", nil
+	// 	} else {
+	// 		return event.SchoolRefID + ":var", nil
+	// 	}
+	// }
+	// resolvers["NAPEvent/SchoolID"] = func(params *graphql.ResolveParams) (interface{}, error) {
+
+	// 	if napEvent, ok := params.Source.(*xml.NAPEvent); ok {
+	// 		return napEvent.SchoolID + ":source", nil
+	// 	} else {
+	// 		return event.SchoolID + ":var", nil
+	// 	}
+	// }
 
 	resolvers["School/events"] = func(params *graphql.ResolveParams) (interface{}, error) {
 
-		return []interface{}{event, event}, nil
+		events := []interface{}{}
+		for _, event := range sd.Events {
+			// if event.TestDisruptionList.TestDisruption != nil {
+			events = append(events, event)
+			// }
+		}
+		return events, nil
 
 	}
 
@@ -76,8 +130,15 @@ func main() {
 	{
 		events {
 			EventID
-			SPRefID
+			#SPRefID
 			PSI
+			SchoolRefID
+			SchoolID
+			TestID
+			NAPTestLocalID
+			TestDisruptionList {
+				Event
+			}
 		}
 	}`
 	result, err := executor.Execute(context, query, variables, "")
